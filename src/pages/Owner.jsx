a407e6bcdecb2eccfa47d15a9d8b78b7a4df49db
Yalbani Aranda <yarandacardona@gmail.com>
2025-06-29 13:04:49 -0600

    added styles

--- src/pages/Owner.jsx
+++ src/pages/Owner.jsx
@@ -17,17 +17,17 @@ return(
       <>
  <HeaderOwner />
    <div> 
-         <img className={style["imgStyle"]} src="/public/images/lady-gaga-brings-mayhem-to-mexico-14.jpg" alt="Lady Gaga en Mexico" /> 
+         <img className={style["imgStyle"]} src="/public/images/306510604_10229553148876836_4362131891721921409_n.jpg" alt="Yalbani Aranda" /> 
        <h2 className={style["h2Style"]}>Hola, soy Yalbani Aranda</h2>   
-       <h4 className={style["h4Style"]}>Soy desarrolladora Fullstack en proceso. <br />
-       Me encanta ver peliculas y Lady Gaga.
+       <h4 className={style["h4Style"]}>Soy desarrolladora Fullstack en proceso. 👩‍💻 <br />
+       Me encanta ver peliculas, el futbol, la musica de los 80s y Lady Gaga 👩‍🎤.
        </h4>
  <br />
     </div>  
-
+<br />
   <div className={style["container"]}>
    <p>Un dato curioso:</p> <br />    
- <h3 className={style["h3Style"]}>Estas son mis películas favoritas!</h3>
+ <h3 className={style["h3Style"]}>Estas son mis películas favoritas! 🎬</h3>
  <br />
  <ol >
         <li className={style["listStyle"]}> In the mouth of Madness </li><br />
@@ -39,15 +39,19 @@ return(
       <br />
           <div className={style["divCont"]}>
          <a className={style["linkStyleL"]} href="https://www.linkedin.com/in/yalbanii/">Mi LinkedIn</a> <br />
-       <a className={style["linkStyleC"]} href="mailto:yarandacardona@gmail.com"> Mándame un correo </a>      
+       <a className={style["linkStyleC"]} href="mailto:yarandacardona@gmail.com"> Mándame un correo 📧 </a>      
+       
     </div>
      <div className={style["containerClic"]}>
       <p>Has hecho click aqui {count} veces</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
+      <button className={style["linkStyleClic"]} onClick={() => setCount(count + 1)}>
+       Haz click en mi 🎯
       </button>
+  
      </div>
-     <br />
+    <img className={style["imgStyle2"]} src="/public/images/lady-gaga-brings-mayhem-to-mexico-14.jpg" alt="Lady Gaga en Mexico" />
+     <img className={style["imgStyle3"]} src="/public/images/erling-haaland-4k-wallpaper-3840x2160-16981.jpeg" alt="Erling Haaland" />
+     
        <FooterOwner />
     </>  
 )
